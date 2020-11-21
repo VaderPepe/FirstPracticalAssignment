b0fb386a2b17dc5c9095aa4b2026f4e96c58aa90
janislapers <73535373+janislapers@users.noreply.github.com>
2020-11-21 16:47:22 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
+  
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      <View style={{padding: 10, justifyContent: 'center',
+      flex: 1
+  }}>
+      <Image
+        style={{height:200, width:'100%'}}
+  source={{uri:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.FJHeaBh6wmWLY4b7sASU_AHaDt%26pid%3DApi&f=1'}}
+        />
+ <Text>Hello World from Group 8 : Ralfs Karlis Lapers</Text>
+        </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    );
+  }
