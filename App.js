80237be8171744b47798cc418c67d7b6472e309f
janislapers <73535373+janislapers@users.noreply.github.com>
2020-11-21 16:47:56 +0200

    Update App.js

--- App.js
+++ App.js
@@ -10,10 +10,10 @@ export default function App() {
       flex: 1
   }}>
       <Image
-        style={{height:200, width:'100%'}}
+        style={{height:100, width:'50%'}}
   source={{uri:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.FJHeaBh6wmWLY4b7sASU_AHaDt%26pid%3DApi&f=1'}}
         />
- <Text>Hello World from Group 8 : Ralfs Karlis Lapers</Text>
+ <Text>Hello World from Group 8 : Jānis Lapers</Text>
         </View>
 
     );
